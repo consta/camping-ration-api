956b6d19f070f80f52b47f7d1bf0fa2e544a84be
Volha <kresiko@gmail.com>
2019-06-05 07:14:53 +0300

    Domain folder is updated (Entities and Repositories are created) in accordance with database model. Dependencies between tables are left out of scope. BUT THERE ARE SOME UNCLEAR ERRORS IN PRODUCTCONTROLLER FILE...

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,6 +1,5 @@
 set referential_integrity false;
 delete from prod_category;
-delete from categories;
 delete from product;
 
 insert into product (id, name_product, calories_product, proteins_product, fats_product, carbohydrates_product, category_of_product) values (10000, 'product #1', 128.5, 12, 5, 45,1);
@@ -9,7 +8,5 @@ insert into product (id, name_product, calories_product, proteins_product, fats_
 
 insert into prod_category (id, name) values (200, 'vegetables');
 insert into prod_category (id, name) values (201, 'fruits');
-insert into prod_category (product_id, category_id) values (10000, 200);
-insert into prod_category (product_id, category_id) values (10000, 201);
-insert into prod_category (product_id, category_id) values (10001, 201);
+
 set referential_integrity true;
