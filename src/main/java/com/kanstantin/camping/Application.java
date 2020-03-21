984fe6e12d4631af56207d23aaa6f33e9e34d790
Kanstantin Kachanouski <kanstantin_kachanouski@epam.com>
2020-03-21 12:31:48 +0300

    Fixed build, updated spring boot version to 2.2.5, added swagger-ui

--- src/main/java/com/kanstantin/camping/Application.java
+++ src/main/java/com/kanstantin/camping/Application.java
@@ -1,6 +1,5 @@
 package com.kanstantin.camping;
 
-import org.dozer.BeanFactory;
 import org.dozer.DozerBeanMapper;
 import org.dozer.Mapper;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
 
@@ -18,12 +22,23 @@ import java.util.Arrays;
 @EnableJpaRepositories(basePackages = {
         "com.kanstantin.camping.domain"
 })
+@EnableSwagger2
 public class Application {
 
     @Bean
     public Mapper mapper() {
         return new DozerBeanMapper(Arrays.asList("mapping.xml"));
     }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
